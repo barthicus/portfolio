8f5ba5381284cc93d3ee175e244acfa22bce0d3c
Bartosz Podgruszecki <bartosz.podgruszecki@gmail.com>
2018-12-26 18:32:25 +0100

    Set prettier and eslint config.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,29 @@
+/* eslint-disable global-require */
 const fs = require('fs')
 const { join } = require('path')
 const { promisify } = require('util')
 const copyFile = promisify(fs.copyFile)
+const withSass = require('@zeit/next-sass')
 
-module.exports = {
-  exportPathMap: async function (defaultPathMap, {dev, dir, outDir, distDir, buildId}) {
-    if (dev) {
-      return defaultPathMap
-    }
-    // copy .nojekyll from project root into the out directory
-    await copyFile(join(dir, '.nojekyll'), join(outDir, '.nojekyll'))
+const exportPathMap = async function (defaultPathMap, {dev, dir, outDir, distDir, buildId}) {
+  if (dev) {
     return defaultPathMap
   }
+  // copy .nojekyll from project root into the out directory
+  await copyFile(join(dir, '.nojekyll'), join(outDir, '.nojekyll'))
+  return defaultPathMap
 }
+
+const webpack = (config, { dev }) => {
+  if (dev) {
+    config.module.rules.push({
+      test: /\.(jsx?|gql|graphql)$/,
+      exclude: ['/node_modules/', '/.next/'],
+      loader: 'eslint-loader',
+      enforce: 'pre'
+    })
+  }
+  return config
+}
+/* eslint-enable global-require */
+module.exports = withSass({ exportPathMap, webpack })

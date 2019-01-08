8a5658a58c534d0f249f67c813031bc9d49133c1
Bartosz Podgruszecki <bartosz.podgruszecki@gmail.com>
2019-01-08 12:07:53 +0100

    Add initial head meta data.

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -8,10 +8,17 @@ import './scss/index.scss';
 const Layout = props => (
   <>
     <Head>
+      <title>Portfolio</title>
+      <meta name="description" content="Moje portfolio." />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#05DFC6" />
       <link
         href="https://fonts.googleapis.com/css?family=Rubik:300,400,500&amp;subset=latin-ext&family=Armata"
         rel="stylesheet"
       />
+      <link rel="icon" href="/static/img/favicon.png" />
+      <link rel="apple-touch-icon-precomposed" href="/static/img/favicon.png" />
     </Head>
     <Header />
     <main>{props.children}</main>

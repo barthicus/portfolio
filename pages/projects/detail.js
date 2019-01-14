6adee0232f131b16a1e75e6f52657765ec200842
Bartosz Podgruszecki <bartosz.podgruszecki@gmail.com>
2019-01-14 23:18:14 +0100

    Add screencasts and code fragments sections.

--- pages/projects/detail.js
+++ pages/projects/detail.js
@@ -1,3 +1,5 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_uid"] }] */
+
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import animateScrollTo from 'animated-scroll-to';
@@ -7,7 +9,7 @@ import StoryblokService from '../../utils/StoryblokService';
 import Layout from '../../components/Layout/index';
 import SvgIcon from '../../components/SvgIcon';
 
-import './project.scss';
+import './detail.scss';
 
 export default class ProjectDetailsPage extends Component {
   static propTypes = {
@@ -19,14 +21,20 @@ export default class ProjectDetailsPage extends Component {
     desktop_view: PropTypes.string,
     tablet_view: PropTypes.string,
     phone_view: PropTypes.string,
-    tags: PropTypes.array
+    online: PropTypes.string,
+    tags: PropTypes.array,
+    code_fragments: PropTypes.array,
+    screencasts: PropTypes.array
   };
 
   static defaultProps = {
     desktop_view: null,
     tablet_view: null,
     phone_view: null,
-    tags: []
+    tags: null,
+    online: null,
+    code_fragments: null,
+    screencasts: null
   };
 
   static async getInitialProps({ asPath, query }) {
@@ -41,6 +49,8 @@ export default class ProjectDetailsPage extends Component {
 
   componentDidMount() {
     StoryblokService.initEditor(this);
+    if (window.location.hash === '') return;
+    setTimeout(() => this.scrollToSection(window.location.hash, 500), 1000);
   }
 
   getMdContent(mdContent, classname) {
@@ -53,26 +63,9 @@ export default class ProjectDetailsPage extends Component {
     );
   }
 
-  JsSnippet = () => {
-    const codeString = `const lul = 'asfsdf';
-  (num) => num + 1;
-  
-  class Testowa {
-    constructor(age) {
-      this.prop = 541 + age;
-    }
-  }
-  
-  testObject = new Testowa(43);`;
-    return (
-      // <SyntaxHighlighter language="javascript" style={docco}>
-      <SyntaxHighlighter language="javascript">{codeString}</SyntaxHighlighter>
-    );
-  };
-
-  scrollToSection = e => {
-    const position = document.querySelector(e.target.hash).offsetTop - 120;
-    animateScrollTo(position, { speed: 200 });
+  scrollToSection = (elSelector, speed = 200) => {
+    const position = document.querySelector(elSelector).offsetTop - 120;
+    animateScrollTo(position, { speed });
   };
 
   render() {
@@ -137,102 +130,94 @@ export default class ProjectDetailsPage extends Component {
                   <div className="text">
                     <p className="text__title">Projekt komercyjny</p>
                     <p className="text__desc">
-                      Podgląd kodu źródłowego dla tego projektu nie jest
-                      dostępny. Zostały jednak wydzielone jego fragmenty oraz
-                      dostępne są screencasty.
+                      {this.props.online &&
+                      this.props.online !== 'commercial' ? (
+                        <>
+                          Strona jest dostępna pod adresem:
+                          <br />
+                          {this.props.online}
+                        </>
+                      ) : (
+                        'Podgląd kodu źródłowego dla tego projektu nie jest dostępny. Zostały jednak wydzielone jego fragmenty oraz dostępne są screencasty.'
+                      )}
                     </p>
                   </div>
                   <div className="buttons">
-                    <a
-                      href="#code-fragments"
-                      onClick={e => this.scrollToSection(e)}
-                      className="button button--outlined"
-                    >
-                      Code
-                    </a>
-                    <a
-                      href="#screencasts"
-                      onClick={e => this.scrollToSection(e)}
-                      className="button button--outlined"
-                    >
-                      Screencasts
-                    </a>
+                    {this.props.online && this.props.online !== 'commercial' && (
+                      <a
+                        href={this.props.online}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="visit-online button"
+                      >
+                        See online
+                      </a>
+                    )}
+                    {this.props.code_fragments.length > 1 && (
+                      <a
+                        href="#code-fragments"
+                        onClick={() => this.scrollToSection('#code-fragments')}
+                        className="button button--outlined"
+                      >
+                        Code
+                      </a>
+                    )}
+                    {this.props.screencasts.length > 1 && (
+                      <a
+                        href="#screencasts"
+                        onClick={() => this.scrollToSection('#screencasts')}
+                        className="button button--outlined"
+                      >
+                        Screencasts
+                      </a>
+                    )}
                   </div>
                 </div>
               </div>
             </div>
-            <SvgIcon icon="colorBar" className="color-bar" />
-            <div className="columns code-fragments">
-              <div className="column">
-                <h2 id="code-fragments">Code examples</h2>
-                <div className="columns">
+            {this.props.code_fragments.length > 1 && (
+              <>
+                <SvgIcon icon="colorBar" className="color-bar" />
+                <div className="columns code-fragments">
                   <div className="column">
-                    {this.JsSnippet()}
-                    <span className="file">/src/components/SomeFile.js</span>
-                  </div>
-                  <div className="column">
-                    {this.JsSnippet()}
-                    <span className="file">
-                      /src/components/SomeOtherFile.js
-                    </span>
-                  </div>
-                  <div className="column">
-                    {this.JsSnippet()}
-                    <span className="file">
-                      /src/components/SomeOtherFile.js
-                    </span>
-                  </div>
-                  <div className="column">
-                    {this.JsSnippet()}
-                    <span className="file">
-                      /src/components/SomeOtherFile.js
-                    </span>
-                  </div>
-                  <div className="column">
-                    {this.JsSnippet()}
-                    <span className="file">
-                      /src/components/SomeOtherFile.js
-                    </span>
+                    <h2 id="code-fragments">Code examples</h2>
+                    <div className="columns">
+                      {this.props.code_fragments.map(fragment => (
+                        <figure className="column" key={fragment._uid}>
+                          <SyntaxHighlighter language={fragment.lang}>
+                            {fragment.content}
+                          </SyntaxHighlighter>
+                          <figcaption className="file">
+                            {fragment.filename}
+                          </figcaption>
+                        </figure>
+                      ))}
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <SvgIcon icon="colorBar" className="color-bar" />
-            <div className="columns screencasts">
-              <div className="column">
-                <h2 id="screencasts">Screencasts</h2>
-                <div className="columns">
-                  <div className="column screencast">
-                    <img
-                      src="/static/img/projects/routes-planner_desktop.png"
-                      alt=""
-                    />
-                    <span className="file">adding order</span>
-                  </div>
-                  <div className="column screencast">
-                    <img
-                      src="/static/img/projects/routes-planner_desktop.png"
-                      alt=""
-                    />
-                    <span className="file">switching tabs</span>
-                  </div>
-                  <div className="column screencast">
-                    <img
-                      src="/static/img/projects/routes-planner_desktop.png"
-                      alt=""
-                    />
-                    <span className="file">switching tabs</span>
-                  </div>
-                  <div className="column screencast">
-                    <img
-                      src="/static/img/projects/routes-planner_desktop.png"
-                      alt=""
-                    />
-                    <span className="file">switching tabs</span>
+              </>
+            )}
+
+            {this.props.screencasts.length > 1 && (
+              <>
+                <SvgIcon icon="colorBar" className="color-bar" />
+                <div className="columns screencasts">
+                  <div className="column">
+                    <h2 id="screencasts">Screencasts</h2>
+                    <div className="columns">
+                      {this.props.screencasts.map(screencast => (
+                        <figure className="column" key={screencast._uid}>
+                          <img src={screencast.source} alt={screencast.title} />
+                          <figcaption className="file">
+                            {screencast.title}
+                          </figcaption>
+                        </figure>
+                      ))}
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
+              </>
+            )}
           </div>
         </section>
       </Layout>

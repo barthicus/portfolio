490a09a69b1d50b40b6d9626b6e0831212422ba2
Bartosz Podgruszecki <bartosz.podgruszecki@gmail.com>
2019-01-20 17:04:23 +0100

    Add RWDInfo component coditional rendering.

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,16 +1,16 @@
-import Head from './Head';
 import Footer from '../Footer';
 import ScrollToTop from '../ScrollToTop';
 import RwdInfo from '../RwdInfo';
 import StoryblokService from '../../utils/StoryblokService';
 import './scss/index.scss';
 
+const isDevMode = process.env.NODE_ENV !== 'production';
+
 const Layout = ({ children }) => (
   <>
-    <Head title="Portfolio" description="Portfolio" />
     <main>{children}</main>
     <ScrollToTop />
-    <RwdInfo />
+    {isDevMode && <RwdInfo />}
     <Footer />
     {StoryblokService.bridge()}
   </>

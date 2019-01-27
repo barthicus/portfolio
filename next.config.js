59c3ea50cf29ab2a615acdc1eb957b1493f1ee78
Bartosz Podgruszecki <bartosz.podgruszecki@gmail.com>
2019-01-27 16:04:59 +0100

    Change plugin next-optimized-images to next-images.

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const copyFile = promisify(fs.copyFile);
 const withPlugins = require('next-compose-plugins');
 const sass = require('@zeit/next-sass');
 const css = require('@zeit/next-css');
-const optimizedImages = require('next-optimized-images');
+const images = require('next-images');
 const axios = require('axios');
 
 const exportPathMap = async (
@@ -56,38 +56,4 @@ const webpack = (config, { dev }) => {
 };
 
 /* eslint-enable global-require */
-module.exports = withPlugins(
-  [
-    [
-      optimizedImages,
-      {
-        // these are the default values so you don't have to provide them if they are good enough for your use-case.
-        // but you can overwrite them here with any valid value you want.
-        inlineImageLimit: 8192,
-        imagesFolder: 'images',
-        imagesName: '[name]-[hash].[ext]',
-        handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'],
-        optimizeImages: true,
-        optimizeImagesInDev: false,
-        mozjpeg: {
-          quality: 80
-        },
-        optipng: {
-          optimizationLevel: 3
-        },
-        pngquant: false,
-        gifsicle: {
-          interlaced: true,
-          optimizationLevel: 3
-        },
-        webp: {
-          preset: 'default',
-          quality: 75
-        }
-      }
-    ],
-    css,
-    sass
-  ],
-  { exportPathMap, webpack }
-);
+module.exports = withPlugins([images, css, sass], { exportPathMap, webpack });

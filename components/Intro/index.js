52dd8ef4dee94728080dbd7f8e4c5f8ef28a2aa3
Bartosz Podgruszecki <bartosz.podgruszecki@gmail.com>
2019-02-08 23:51:44 +0100

    Fix h1 semantics.

--- components/Intro/index.js
+++ components/Intro/index.js
@@ -5,8 +5,10 @@ import './index.scss';
 const Intro = () => (
   <div className="main-intro bg-with-pattern">
     <div className="container container--column">
-      <h1 className="main-intro__title">Front-End Developer</h1>
-      <h2 className="main-intro__subtitle">Bartosz Podgruszecki</h2>
+      <h1 className="main-intro__title">
+        Front-End Developer
+        <span className="main-intro__subtitle">Bartosz Podgruszecki</span>
+      </h1>
       <Link href="/about-me">
         <a className="main-intro__link button">About me</a>
       </Link>

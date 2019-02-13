3eaea3a63b0bf97b72482674ee10046d3ec868f6
Bartosz Podgruszecki <bartosz.podgruszecki@gmail.com>
2019-02-13 23:19:36 +0100

    Remove old code.

--- components/Header/index.spec.js
+++ components/Header/index.spec.js
@@ -5,11 +5,6 @@ import React from 'react';
 
 import Header from './index';
 
-describe('With Enzyme', () => {
-  it('renders header', () => {
-    const header = shallow(<Header />);
-
-    expect(header.find('.navbar')).toBeDefined();
-    // expect(header.find('.navbar').text()).toEqual('BARTOSZ PODGRUSZECKI');
-  });
+it('renders without crashing', () => {
+  shallow(<Header />);
 });

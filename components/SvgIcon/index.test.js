0717facbd96ebac45511a21e604eef33a7b78470
Bartosz Podgruszecki <bartosz.podgruszecki@gmail.com>
2019-02-13 23:51:50 +0100

    Add more tests.

--- components/SvgIcon/index.test.js
+++ components/SvgIcon/index.test.js
@@ -1,10 +1,20 @@
 /* eslint-env jest */
 
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import React from 'react';
 
 import SvgIcon from './index';
 
 it('renders without crashing', () => {
-  shallow(<SvgIcon />);
+  shallow(<SvgIcon icon="test" />);
+});
+
+it('includes text label if no svg icon was found', () => {
+  const svgIcon = shallow(<SvgIcon icon="test" />);
+  expect(svgIcon.find('.no-svg').text()).toEqual('test');
+});
+
+it('includes SVG element if svg icon was found', () => {
+  const svgIcon = mount(<SvgIcon icon="arrowUp" />);
+  expect(svgIcon.find('svg')).toHaveLength(1);
 });

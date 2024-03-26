def5f9567bf7b41a561f476e031a0c072eae54ec
Bartosz Podgruszecki <bartosz.podgruszecki@gmail.com>
2024-03-26 11:14:05 +0100

    Disable download function in cv pdf

--- app/cv-pdf/route.tsx
+++ app/cv-pdf/route.tsx
@@ -3,8 +3,8 @@ import { renderToBuffer } from '@react-pdf/renderer'
 
 import { getCVContent } from './getCVContent'
 
-export const GET = async (request: Request) => {
-  const shouldDownload = request.headers.get('download') === 'true'
+export const GET = async (_request: Request) => {
+  // const shouldDownload = request.headers.get('download') === 'true'
 
   const cvContent = getCVContent()
 
@@ -13,9 +13,9 @@ export const GET = async (request: Request) => {
   const headers = new Headers()
   headers.set('Content-Type', 'application/pdf')
 
-  if (shouldDownload) {
-    headers.set('Content-Disposition', `attachment; filename="Bartosz_Podgruszecki_CV.pdf"`)
-  }
+  // if (shouldDownload) {
+  //   headers.set('Content-Disposition', `attachment; filename="Bartosz_Podgruszecki_CV.pdf"`)
+  // }
 
   return new NextResponse(buffer, { headers })
 }

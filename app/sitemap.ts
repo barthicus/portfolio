bfc02ebdbb093318a4d999ac7f5318ef817d1590
Bartosz Podgruszecki <bartosz.podgruszecki@gmail.com>
2024-04-26 14:27:12 +0200

    Fix dates in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,17 +7,17 @@ const HOST_URL = process.env.NODE_ENV === 'development' ? LOCAL_URL : VERCEL_URL
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const home = {
     url: HOST_URL,
-    lastModified: new Date().toString()
+    lastModified: new Date()
   }
 
   const allProjects = {
     url: `${HOST_URL}/all-projects/grid`,
-    lastModified: new Date().toString()
+    lastModified: new Date()
   }
 
   const cvPdf = {
     url: `${HOST_URL}/cv-pdf`,
-    lastModified: new Date().toString()
+    lastModified: new Date()
   }
 
   return [home, allProjects, cvPdf]
